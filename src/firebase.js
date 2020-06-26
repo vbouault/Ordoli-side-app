718ab87e2d2ab432c3f0c71043118be746288137
Vianney Bouault <vbouault@hotmail.fr>
2020-06-26 07:10:18 +0200

    ...

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBkWIgJeGqc5unvAPHfXNXjPOlQti5-aUE",
-  authDomain: "hackathon-dev2020.firebaseapp.com",
-  databaseURL: "https://hackathon-dev2020.firebaseio.com",
-  projectId: "hackathon-dev2020",
-  storageBucket: "hackathon-dev2020.appspot.com",
-  messagingSenderId: "739471949",
-  appId: "1:739471949:web:9aee5beb6b14f4baeca204",
-  measurementId: "G-NR0G2XFMNG"
+  apiKey: 'AIzaSyBkWIgJeGqc5unvAPHfXNXjPOlQti5-aUE',
+  authDomain: 'hackathon-dev2020.firebaseapp.com',
+  databaseURL: 'https://hackathon-dev2020.firebaseio.com',
+  projectId: 'hackathon-dev2020',
+  storageBucket: 'hackathon-dev2020.appspot.com',
+  messagingSenderId: '739471949',
+  appId: '1:739471949:web:197b7499d9cbf1d7eca204',
+  measurementId: 'G-EKPJZCQMTP'
 };
 
 firebase.initializeApp(firebaseConfig);
